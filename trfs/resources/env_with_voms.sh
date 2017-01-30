44732eacb55429df450d8300c9d7ec4d1d0916ac
lukasheinrich <lukas.heinrich@gmail.com>
2017-01-30 10:32:52 +0100

    Revert "repush"
    This reverts commit ca4d763158dfd0221307d058aa5bb01346c8d302.

---
+++ trfs/resources/env_with_voms.sh
@@ -0,0 +1,9 @@
+export ATLAS_LOCAL_ROOT_BASE="/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase"
+setupATLAS='source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh'
+alias setupATLAS='source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh'
+alias asetup='source $AtlasSetup/scripts/asetup.sh'
+setupATLAS
+asetup $1
+cd /recast_auth
+source ./getmyproxy.sh
+cd -
