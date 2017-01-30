ca4d763158dfd0221307d058aa5bb01346c8d302
lukasheinrich <lukas.heinrich@gmail.com>
2017-01-30 10:23:47 +0100

    repush

--- evgen/resources/env.sh
+++
@@ -1,7 +0,0 @@
-export ATLAS_LOCAL_ROOT_BASE="/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase"
-setupATLAS='source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh'
-alias setupATLAS='source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh'
-alias asetup='source $AtlasSetup/scripts/asetup.sh'
-setupATLAS
-asetup MCProd,19.2.5.3.3,here
-
--- trfs/resources/env_with_voms.sh
+++
@@ -1,9 +0,0 @@
-export ATLAS_LOCAL_ROOT_BASE="/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase"
-setupATLAS='source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh'
-alias setupATLAS='source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh'
-alias asetup='source $AtlasSetup/scripts/asetup.sh'
-setupATLAS
-asetup $1
-cd /recast_auth
-source ./getmyproxy.sh
-cd -
